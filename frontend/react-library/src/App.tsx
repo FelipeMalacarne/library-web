8c6814229d1fa4542fc3d1407fea0aa388b381bf
FelipeMalacarne <felipemalacarne012@gmail.com>
2023-03-16 01:41:25 -0300

    setup for react frontend

---
+++ frontend/react-library/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to de fullstack library application</h1>
+    </div>
+  );
+}
+
+export default App;
