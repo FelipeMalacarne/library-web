b2f70e355a86e36f58f8f0b0b5ab51d511ed46fb
FelipeMalacarne <felipemalacarne012@gmail.com>
2023-03-16 13:29:44 -0300

    React Homepage, navbar, carousel, topBooks

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to de fullstack library application</h1>
+    <div>
+      <Navbar/>
+      <ExploreTopBooks/>
+      <Carousel/>
     </div>
   );
 }
---
+++ frontend/react-library/src/layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,14 @@
+export const ExploreTopBooks = () => {
+    return (
+        <div className="p-5 mb-4 bg-dark header">
+            <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center d-flex">
+                <div>
+                    <h1 className="display-5 fw-bold">Encontre sua próxima aventura</h1>
+                    <p className="col-md-8 fs-4">Onde você gostaria de ir?</p>
+                    <a type="button" className="btn main-color btn-lg text-white" href="#">
+                        Explore os livros</a>
+                </div>
+            </div>
+        </div>
+    )
+}
