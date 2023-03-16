3992e8a3a1919d4edbbd960e5599a153795624b7
FelipeMalacarne <felipemalacarne012@gmail.com>
2023-03-16 13:43:11 -0300

    ReturnBook Component

---
+++ frontend/react-library/src/layouts/HomePage/ReturnBook.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const ReturnBook = () => {
+    return (
+        <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+            <div className="text-center">
+                <img src={require('./../../Images/BooksImages/livro-lotr-pt1.jpg')}
+                    width='151'
+                    height='233'
+                    alt="book" />
+                <h6 className="mt-2">Book</h6>
+                <p>Tolkien</p>
+                <a className="btn main-color text-white" href="#">Reserve</a>
+            </div>
+        </div>
+    );
+}
