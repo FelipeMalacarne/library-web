83358509f4177bc20ea64becff2ac2ee26a46004
FelipeMalacarne <felipemalacarne012@gmail.com>
2023-03-22 10:36:12 -0300

    React Keep Footer on the bottom

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -8,23 +8,25 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <div>
+    <div className='d-flex flex-column min-vh-100'>
       <Navbar/>
 
-      <Switch>
+      <div className='flex-grow-1'>
+        <Switch>
 
-        <Route path='/' exact>
-          <Redirect to='/home'/>
-        </Route>
+          <Route path='/' exact>
+            <Redirect to='/home'/>
+          </Route>
 
-        <Route path='/home'>
-          <HomePage/>
-        </Route>
+          <Route path='/home'>
+            <HomePage/>
+          </Route>
 
-        <Route path='/search'>
-          <SearchBooksPage/>
-        </Route>
-      </Switch>
+          <Route path='/search'>
+            <SearchBooksPage/>
+          </Route>
+        </Switch>
+      </div>
 
       <Footer/>
     </div>
