cf98a9633fe2ff87ba34544c22576ee9188f0c66
FelipeMalacarne <felipemalacarne012@gmail.com>
2023-03-23 23:54:05 -0300

    Spring Review entity

--- backend/spring-boot-library/src/main/java/com/demolib/springbootlibrary/entity/Book.java
+++ backend/spring-boot-library/src/main/java/com/demolib/springbootlibrary/entity/Book.java
@@ -13,7 +13,7 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
-    private Integer id;
+    private Long id;
 
     @Column(name="title")
     private String title;
