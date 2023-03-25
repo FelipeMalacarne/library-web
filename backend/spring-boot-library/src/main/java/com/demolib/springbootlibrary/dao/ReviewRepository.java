0c42e572ff0e9f4479f442c4a6f07f81dc6404c9
FelipeMalacarne <felipemalacarne012@gmail.com>
2023-03-25 18:42:57 -0300

    Spring Find Review by ID

--- backend/spring-boot-library/src/main/java/com/demolib/springbootlibrary/dao/ReviewRepository.java
+++ backend/spring-boot-library/src/main/java/com/demolib/springbootlibrary/dao/ReviewRepository.java
@@ -1,8 +1,12 @@
 package com.demolib.springbootlibrary.dao;
 
 import com.demolib.springbootlibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
 }
