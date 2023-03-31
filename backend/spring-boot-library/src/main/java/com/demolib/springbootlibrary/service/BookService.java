7456d5d33195c609149432e1d4525a30d1b55fcb
FelipeMalacarne <felipemalacarne012@gmail.com>
2023-03-31 14:58:42 -0300

    Spring BookService

---
+++ backend/spring-boot-library/src/main/java/com/demolib/springbootlibrary/service/BookService.java
@@ -0,0 +1,51 @@
+package com.demolib.springbootlibrary.service;
+
+import com.demolib.springbootlibrary.dao.BookRepository;
+import com.demolib.springbootlibrary.dao.CheckoutRepository;
+import com.demolib.springbootlibrary.entity.Book;
+import com.demolib.springbootlibrary.entity.Checkout;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class BookService {
+
+    private BookRepository bookRepository;
+    private CheckoutRepository checkoutRepository;
+
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+        this.bookRepository = bookRepository;
+        this.checkoutRepository = checkoutRepository;
+    }
+
+    public Book checkoutBoot(String userEmail, Long bookId) throws Exception {
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if(!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0){
+            throw new Exception("Book doesn't exist or already has been checked out by the user");
+        }
+
+        // Updates DB
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
+        bookRepository.save(book.get());
+
+        Checkout checkout = new Checkout(
+                userEmail,
+                LocalDate.now().toString(),
+                LocalDate.now().plusDays(7).toString(),
+                book.get().getId()
+        );
+
+        checkoutRepository.save(checkout);
+
+        return book.get();
+    }
+}
+
+
