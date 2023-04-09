d8b0d0129562f3e52e8de80eeba5c135d5b49bae
FelipeMalacarne <felipemalacarne012@gmail.com>
2023-04-08 22:19:15 -0300

    Spring Renew Loan Service]

--- backend/spring-boot-library/src/main/java/com/demolib/springbootlibrary/service/BookService.java
+++ backend/spring-boot-library/src/main/java/com/demolib/springbootlibrary/service/BookService.java
@@ -113,6 +113,24 @@ public class BookService {
         checkoutRepository.deleteById(validateCheckout.getId());
     }
 
+    public void renewLoan(String userEmail, Long bookId) throws Exception{
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if(validateCheckout == null){
+            throw new Exception("Book does not exist or not checked out by the user");
+        }
+
+        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date d1 = sdFormat.parse(validateCheckout.getReturnDate());
+        Date d2 = sdFormat.parse(LocalDate.now().toString());
+
+        if(d1.compareTo(d2) > 0 || d1.compareTo(d2) == 0){
+            validateCheckout.setReturnDate(LocalDate.now().plusDays(7).toString());
+            checkoutRepository.save((validateCheckout));
+        }
+    }
+
 }
 
 
