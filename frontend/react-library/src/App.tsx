2437b21bd7f1fe76b95070d57a3981edd281ed0f
FelipeMalacarne <felipemalacarne012@gmail.com>
2023-04-13 01:16:48 -0300

    React Messages Component

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -12,6 +12,7 @@ import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
+import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -65,6 +66,10 @@ export const App = () => {
 
           <SecureRoute path={'/shelf'}>
               <ShelfPage/>
+          </SecureRoute>
+
+          <SecureRoute path={'/messages'}>
+            <MessagesPage/>
           </SecureRoute>
 
         </Switch>
---
+++ frontend/react-library/src/layouts/MessagesPage/MessagesPage.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const MessagesPage = () =>{
+  
+  const[messagesClick, setMessagesClick] = useState(false);
+
+  return (
+    <div className="container">
+      <div className="mt-3 mb-2">
+        <nav>
+          <div className="nav nav-tabs" id="nav-tab" role="tablist">
+            <button onClick={() => setMessagesClick(false)} className="nav-link active" id="nav-send-message-tab" data-bs-toggle='tab' data-bs-target='#nav-send-message' type="button" role="tab" aria-controls="nav-send-message" aria-selected='true'>
+              Enviar Pergunta
+            </button>
+            <button onClick={() => setMessagesClick(true)} className="nav-link" id="nav-message-tab" data-bs-toggle='tab' data-bs-target="#nav-message" type="button" role="tab" aria-controls="nav-message" aria-selected='false'>
+              Perguntas e respostas
+            </button>
+          </div>
+        </nav>
+        <div className="tab-content" id="nav-tabContent">
+          <div className="tab-pane fade show active" id="nav-send-message" role="tabpanel" aria-labelledby="nav-send-message-tab">
+            <p>Poste uma nova pergunta</p>
+          </div>
+          <div className="tab-pane fade" id="nav-message" role="tabpanel" aria-labelledby="nav-message-tab">
+            {messagesClick ? <p>Perguntas</p> : <></>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
